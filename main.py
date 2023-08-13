7a6788f5e1e8e52387a4358bab856b1e45b80475
Rohan <rohanupadhyay@Rohans-MacBook-Air-2.local>
2023-08-13 00:26:29 -0500

    added multiply.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 import addition
 import subtraction
+import multiply
 
 def run(arg, num1, num2):
 
 	addObject = addition.Addition(num1, num2)
 	subObject = subtraction.Subtraction(num1, num2)
+	multObject = multiply.Multiplication(num1, num2)
 
 	result = 0
 
@@ -18,7 +20,11 @@ def run(arg, num1, num2):
 
 	elif arg == "sub":
 		result = subObject.subtract()
-		resultPrinted = "The added numbers are: " + str(result)
+		resultPrinted = "The subtracted numbers are: " + str(result)
+
+	elif arg == "mult":
+		result = multObject.multiply()
+		resultPrinted = "The multiplied numbers are: " + str(result)
 
 	return resultPrinted
 
@@ -26,7 +32,7 @@ def run(arg, num1, num2):
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-arg = str(input("What do you wanna do? (add, sub): "))
+arg = str(input("What do you wanna do? (add, sub, mult): "))
 
 print(run(arg, num1, num2))
 
