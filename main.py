ef0a7b523c571979f2d70a2d4a16dd2ccb210e84
Swoyam Thapa <thapaswoyam123@gmail.com>
2023-08-13 10:54:46 -0500

    modulo added

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import addition
 import subtraction
 import division
 import multiply
+import modulo
 
 
 def run(arg, num1, num2):
 
 	addObject = addition.Addition(num1, num2)
 	subObject = subtraction.Subtraction(num1, num2)
-
 	divideObject = division.Division(num1,num2)
-
 	multObject = multiply.Multiplication(num1, num2)
+	moduloObject = modulo.Modulo(num1, num2)
 
 
 	result = 0
@@ -35,6 +35,10 @@ def run(arg, num1, num2):
 	elif arg == "divide":
 		result = divideObject.divide()
 		resultPrinted = "The division of numbers is: " + str(result)
+	
+	elif arg == "modulo":
+		result = moduloObject.modulo()
+		resultPrinted = "The modulo of numbers is: " + str(result)
 
 	return resultPrinted
 
@@ -42,7 +46,7 @@ def run(arg, num1, num2):
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-arg = str(input("What do you wanna do? (add, sub, mult, divide): "))
+arg = str(input("What do you wanna do? (add, sub, mult, divide, modulo): "))
 
 print(run(arg, num1, num2))
 
