cb5314d09f24abc42c9168145b2acb615691d1a8
Swoyam Thapa <thapaswoyam123@gmail.com>
2023-08-13 00:06:43 -0500

    The addition class

---
+++ addition.py
@@ -0,0 +1,9 @@
+# this module is for the addition function for the calculator app
+
+class Addition:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
